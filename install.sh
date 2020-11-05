edc238ef14e7e5555c8dc0c35f78ac45934a0ec4
sushma-gurunule <70726241+sushma-gurunule@users.noreply.github.com>
2020-11-05 16:46:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo yum update
 sudo yum install -y httpd
 sudo chkconfig httpd on
 sudo service httpd start
-echo "<h1>Deployed via Terraform wih ALB</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Hellow From Sushma Gurunule</h1>" | sudo tee /var/www/html/index.html
