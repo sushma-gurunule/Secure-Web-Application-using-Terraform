cd465585c41426737d2bea4f5020e66a74a52ef3
sushma-gurunule <70726241+sushma-gurunule@users.noreply.github.com>
2020-11-05 16:45:20 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
          region = "us-east-1"
-         access_key = "AKIAR4LVUUMH7OPAXXRT"
-         secret_key = "sg/8LA/RFNwbX87ecsQ7++PxkS530VYGxt7yLlgN"
+         access_key = "Enter_Your_AWS_IAM_User_Access_Key"
+         secret_key = "Enter_Your_AWS_IAM_User_Secret_Access_Key"
 }
 
